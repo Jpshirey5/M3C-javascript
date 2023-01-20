7439e46573464ac706aba2536bd309336371ac7f
John Shirey <you@example.com>
2023-01-19 22:58:05 -0500

    added JS Code

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,5 +1,32 @@
 // Assignment code here
 
+//Password
+function generatePassword() {
+  console.log ("Click to create password");
+  let pass = "";
+}
+
+//Prompts
+var passwordLength = window.prompt ("Password Length");
+console.log (passwordLength);
+
+var lowercase = window.prompt("Would you like to have lowercase letters?");
+console.log(lowercase);
+
+var uppercase = window.prompt("Would you like to have upppercase letters?");
+console.log(uppercase);
+
+var numbers = window.prompt("Would you like to add numbers?")
+console.log(numbers);
+
+var characters = window.prompt("Would you like to add specialcharacters?")
+console.log(characters);
+
+//Variables
+var uppercaseletters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ".split("");
+var lowercaseletters = "abcdefghijklmnopqrstuvwxyz ".split("");
+var numbers = "0123456789 ".split("");
+var characters = "!@#$%^&*+=?/;: ".split("");
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
